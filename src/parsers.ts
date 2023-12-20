c73c6acb3ed0a0232bd1948609f7e4add3c854b9
Harry <42577440+AsianLazyMan@users.noreply.github.com>
2023-12-20 11:39:38 +0800

    Add tubor main program code (#1)
    * Add tubor main program code

---
+++ src/parsers.ts
@@ -0,0 +1,21 @@
+import { xml2json } from 'xml-js';
+
+interface TranscriptObject {
+    text: string;
+    start: number;
+    dur: number;
+}
+
+export function transcriptParser(input: string): TranscriptObject[] {
+    const jsonData = JSON.parse(xml2json(input, { compact: true, spaces: 2 }));
+    const transcript = jsonData.transcript;
+    const textArray = Array.isArray(transcript.text) ? transcript.text : [transcript.text];
+
+    return textArray.map((textData: any) => ({
+        // Use the regular expression /\n/g and /\t/g to replace \n and \t with the empty string ''
+        text: textData._text.replace(/\n/g, '').replace(/\t/g, ''),
+        // If the start or dur is null or undefined use 0.0 as the default value
+        start: parseFloat(textData._attributes.start) || 0.0,
+        dur: parseFloat(textData._attributes.dur) || 0.0
+    }));
+}
---
+++ src/tubor.error.ts
@@ -0,0 +1,49 @@
+export const ExceptionCode = {
+    EMPTY_URL: 'TURBO.BASIC.0001',
+    REQUEST_ERROR: 'TURBO.BASIC.0002',
+    MATCH_ERROR: 'TURBO.BASIC.0003',
+    EXTRACT_ERROR: 'TURBO.BASIC.0004',
+    DEFAULT_ERROR: 'TURBO.BASIC.9999'
+} as const;
+
+export class TuborBasicError extends Error {
+    protected map: Map<string, string>;
+    protected code: string = '';
+    protected msg: string | undefined = '';
+    protected detail: string = '';
+    constructor(code: string = 'BASIC9999', detail: string = '') {
+        super()
+        this.map = new Map([
+            ['TURBO.BASIC.0001', 'Empty url'],
+            ['TURBO.BASIC.0002', 'Request error'],
+            ['TURBO.BASIC.0003', 'Video not found'],
+            ['TURBO.BASIC.0004', 'Extract html error'],
+            ['TURBO.BASIC.9999', 'Default tuborBasicError'],
+        ])
+        this.detail = detail;
+        this.check(code, detail);
+    }
+
+    protected check(
+        code: string = 'BASIC9999',
+        detail: string = '',
+    ) {
+        this.detail = detail;
+        if (this.map.has(code)) {
+            this.code = code;
+            this.msg = this.map.get(code);
+        } else {
+            this.code = 'BASIC9999';
+            this.msg = this.map.get(code);
+        }
+    }
+
+}
+
+export class InvalidInputError extends TuborBasicError {}
+
+export class VideoNotFoundError extends TuborBasicError {}
+
+export class ExtractHtmlError extends TuborBasicError {}
+
+export class AxiosRequestError extends TuborBasicError {}
