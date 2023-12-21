9c7aeb8fd943fb4a9c0d6571bfc5ac93b19c0e69
Harry <42577440+AsianLazyMan@users.noreply.github.com>
2023-12-21 15:03:59 +0800

    Unescape the obtained text (#4)

--- src/parsers.ts
+++ src/parsers.ts
@@ -1,3 +1,4 @@
+import { unescape } from 'he';
 import { xml2json } from 'xml-js';
 
 interface TranscriptObject {
@@ -13,7 +14,7 @@ export function transcriptParser(input: string): TranscriptObject[] {
 
   return textArray.map((textData: any) => ({
     // Use the regular expression /\n/g and /\t/g to replace \n and \t with the empty string ''
-    text: textData._text.replace(/\n/g, '').replace(/\t/g, ''),
+    text: unescape(textData._text.replace(/\n/g, '').replace(/\t/g, '')),
     // If the start or dur is null or undefined use 0.0 as the default value
     start: parseFloat(textData._attributes.start) || 0.0,
     dur: parseFloat(textData._attributes.dur) || 0.0,
