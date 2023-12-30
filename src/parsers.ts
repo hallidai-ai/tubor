ae171c95ddbc091f69c6d70d9768268f5ebeee72
Harry <42577440+AsianLazyMan@users.noreply.github.com>
2023-12-30 17:07:48 +0800

    Modify line breaks and tab replacement

--- src/parsers.ts
+++ src/parsers.ts
@@ -13,8 +13,8 @@ export function transcriptParser(input: string): TranscriptObject[] {
   const textArray = Array.isArray(transcript.text) ? transcript.text : [transcript.text];
 
   return textArray.map((textData: any) => ({
-    // Use the regular expression /\n/g and /\t/g to replace \n and \t with the empty string ''
-    text: textData._text ? unescape(textData._text.replace(/\n/g, '').replace(/\t/g, '')) : '',
+    // Use the regular expression /\n/g and /\t/g to replace \n and \t with the string ' '
+    text: textData._text ? unescape(textData._text.replace(/\n/g, ' ').replace(/\t/g, ' ')) : '',
     // If the start or dur is null or undefined use 0.0 as the default value
     start: parseFloat(textData._attributes.start) || 0.0,
     dur: parseFloat(textData._attributes.dur) || 0.0,
